f1de0518fb9d2dedcfc511570849b2e98c11ae79
Yannick KIKI B <seyive.kiki@gmail.com>
2019-05-17 12:57:02 +0100

    New feature selection technique

--- mobile_price_classification/mpc.py
+++ mobile_price_classification/mpc.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.feature_selection import SelectKBest, chi2, RFE
+from sklearn.linear_model import LogisticRegression
 import seaborn as sns
 
 data = pd.read_csv("train.csv")
@@ -13,9 +14,9 @@ fit = bestfeatures.fit(X,y)
 dfscores = pd.DataFrame(fit.scores_)
 dfcolumns = pd.DataFrame(X.columns)
 #concat two dataframes for better visualization 
-featureScores = pd.concat([dfcolumns,dfscores],axis=1)
-featureScores.columns = ['Specs','Score']  #naming the dataframe columns
-featureScores.nlargest(10,'Score')  #print 10 best features
+f1 = pd.concat([dfcolumns,dfscores],axis=1)
+f1.columns = ['Specs','Score']  #naming the dataframe columns
+f1.nlargest(10,'Score')  #print 10 best features
 
 #2
 from sklearn.ensemble import ExtraTreesClassifier
@@ -23,7 +24,8 @@ import matplotlib.pyplot as plt
 model = ExtraTreesClassifier()
 model.fit(X,y)
 feat_importances = pd.Series(model.feature_importances_, index=X.columns)
-feat_importances.nlargest(10).plot(kind='barh')
+f2 = feat_importances.nlargest(10)
+f2.plot(kind='barh')
 plt.show()
 
 #3
@@ -33,3 +35,15 @@ top_corr_features = corrmat.index
 plt.figure(figsize=(20,20))
 #plot heat map
 g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
+
+#4
+model = LogisticRegression()
+# create the RFE model and select 3 attributes
+rfe = RFE(model, 10)
+rfe = rfe.fit(X, y)
+# summarize the selection of the attributes
+rfe.support_
+rfe.ranking_
+f3 = list(enumerate(X.columns))
+#rank features
+f3.sort(key = lambda idx_feature: rfe.ranking_[idx_feature[0]], reverse = True)
