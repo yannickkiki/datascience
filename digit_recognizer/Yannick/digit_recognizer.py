1c68ec0633950be4460b5588c6e94a5bf56091a0
sojeba008 <34401757+sojeba008@users.noreply.github.com>
2019-05-29 11:26:44 +0100

    Digit-recognizer

--- digit_recognizer/Yannick/digit_recognizer.py
+++ digit_recognizer/Yannick/digit_recognizer.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 #read data
 df = pd.read_csv("train.csv")
-X = df.drop("label", axis=1).values.astype(float)
-X /= 255.0
+X = df.drop("label", axis=1).values
 X = X.reshape(X.shape[0],28,28,1)
 y = df["label"].values
 y = y.reshape(y.shape[0], 1)
@@ -18,7 +17,6 @@ y = onehotencoder.fit_transform(y).toarray()
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-del X, y
 
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
@@ -43,28 +41,19 @@ model.add(Dense(units = 10, activation = 'softmax'))
 model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 #Fitting the CNN
-model.fit(X_train, y_train, batch_size = 256, epochs = 20)
+model.fit(X_train, y_train, batch_size = 1024, epochs = 1)
 
 #Evaluate the model
 model.evaluate(X_train, y_train, verbose=False)
 model.evaluate(X_test, y_test, verbose=False)
 
-"""
-#Save the model if good
-pd.to_pickle(model, "model0.985.sv")
-
-#Load a model
-model = pd.read_pickle("model0.983.sv")
-"""
-
 #Make prediction on validation data
 validation_X = pd.read_csv("test.csv")
-validation_X = validation_X.values.astype(float)
-validation_X /= 255
+validation_X = validation_X.values
 validation_X = validation_X.reshape(validation_X.shape[0],28,28,1)
 validation_y = model.predict(validation_X)
 validation_data = {"ImageId" : list(range(1,1+validation_X.shape[0])),
                    "Label" : [list(line).index(max(line)) for line in validation_y]}
 validation_df = pd.DataFrame.from_dict(validation_data)
 validation_df.set_index("ImageId",inplace=True)
-validation_df.to_csv("submission0.983.csv")
+validation_df.to_csv("submission0.csv")
--- others/yannick/transfer-learning-for-computer-vision/save-pre-trained-model.py
+++
@@ -1,9 +0,0 @@
-from keras.applications.nasnet import NASNetMobile
-from keras.preprocessing import image
-from keras.applications.nasnet import preprocess_input, decode_predictions
-import numpy as np
-import pandas as pd
-
-model = NASNetMobile(weights='imagenet')
-pd.to_pickle(model, "nasnetmobile.model")
-
